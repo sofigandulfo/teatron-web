eefb081e277eb3cf6a4f911dbec1da43785a9d8c
sofigandulfo <sofigandulfo@gmail.com>
2024-04-23 17:48:57 -0300

    Configuración de la Base de Datos y server

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import ProyectoController from './controllers/ProyectoControllers.js';
+import ReservaController from './controllers/ReservaController.js';
+
+const app = express();
+
+const PORT = process.env.PORT ?? 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('¡Backend de Teatron en funcionamiento!')
+});
+
+// proyectos
+app.get('/proyectos/:id', ProyectoController.obtenerPorId)
+
+
+//reservas
+app.get('/reservas/:proyecto_id', ReservaController.obtenerReservasPorProyecto);
+app.post('/reservas/:proyecto_id', ReservaController.crearReserva);
+
+app.listen(PORT, () => {
+    console.log(`Servidor en ejecución en http://localhost:${PORT}`)
+})
+
