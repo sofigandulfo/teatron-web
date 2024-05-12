fb42b6289e03c422500834d6dc7baed9e9a05995
sofigandulfo <sofigandulfo@gmail.com>
2024-05-12 16:09:18 -0300

    Agregado ProjectCard y ProjectCarrousel + estilos

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,14 +1,23 @@
+import "../../styles/ProjectCarrousel.css";
+import img from "../../assets/caretas.avif";
 
-
-function ProjectCard() {
+function ProjectCard({ project }) {
   return (
-    <article>
-        <img src="" alt=""></img>
-        <h2>Nombre del proyecto</h2>
-        <h3>Tipo de proyecto</h3>
-        <button>Ver más</button>
-    </article>
-  )
+    <>
+      <article className="project-card" key={project.id}>
+        <picture className="project-card-picture">
+          <img className="project-card-img" src={img} alt={project.title} />
+        </picture>
+        <div className="project-card-data">
+          <div className="project-card-info">
+            <h2 className="project-card-name">{project.nombre}</h2>
+            <h3 className="project-card-type">{project.tipo}</h3>
+          </div>
+          <button className="project-card-button">VER MÁS</button>
+        </div>
+      </article>
+    </>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
--- src/components/ProjectCarrousel/ProjectCarrousel.js
+++ src/components/ProjectCarrousel/ProjectCarrousel.js
@@ -1,16 +1,51 @@
-import ProjectCard from "../ProjectCard/ProjectCard"
-
+import Slider from "react-slick";
+import proyectos from '../../data/proyectos.json';
+import "../../styles/ProjectCarrousel.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ProjectCard from "../ProjectCard/ProjectCard";
 
 function ProjectCarrousel() {
-  return (
-    <div>
-        <h2>Proyectos</h2>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="project-carrousel">
+      <h3 className="project-carrousel-title">PROYECTOS</h3>
+      <Slider {...settings}>   
+        {proyectos.map((project) => (
+          <div key={project.id}> {/* Cada tarjeta debe estar dentro de un elemento de carrusel separado */}
+            <ProjectCard project={project} />
+          </div>
+        ))}
+      </Slider>
     </div>
-  )
+  );
 }
 
-export default ProjectCarrousel
+export default ProjectCarrousel;
