47ae1c29df0dd5c05003a22b678639f6e510149d
sofigandulfo <sofigandulfo@gmail.com>
2024-05-12 16:07:57 -0300

    Agregado de Footer + estilos

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import Instagram from '../../assets/icons/Instagram';
+import '../../styles/Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+        <p>Copyright © 2024 Elenco Teconté</p>
+        <Instagram />
+    </footer>
+  )
+}
+
+export default Footer
