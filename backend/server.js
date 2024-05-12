ade897dba657541274a3a871a3f055786be5b168
sofigandulfo <sofigandulfo@gmail.com>
2024-05-12 16:09:51 -0300

    Agregado de cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import ProyectoController from './controllers/ProyectoControllers.js';
+import cors from 'cors';
+import ProyectoController from './controllers/ProyectoController.js';
 import ReservaController from './controllers/ReservaController.js';
 
 const app = express();
 
 const PORT = process.env.PORT ?? 5000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
