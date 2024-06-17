a7499d9fdb33ae1c1a6b4ae6d7841e84179b3c6a
sofigandulfo <sofigandulfo@gmail.com>
2024-06-17 01:37:11 -0300

    ProjectCard component

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,19 +1,23 @@
+import { Link } from "react-router-dom";
 import "../../styles/ProjectCarrousel.css";
 import img from "../../assets/caretas.avif";
+
 
 function ProjectCard({ project }) {
   return (
     <>
       <article className="project-card" key={project.id}>
         <picture className="project-card-picture">
-          <img className="project-card-img" src={img} alt={project.title} />
+          <img className="project-card-img" src={img} alt={`imgen del proyecto: ${project.title}`} />
         </picture>
         <div className="project-card-data">
           <div className="project-card-info">
             <h2 className="project-card-name">{project.nombre}</h2>
             <h3 className="project-card-type">{project.tipo}</h3>
           </div>
-          <button className="project-card-button">VER MÁS</button>
+          <Link to={`/proyectos/${project.nombre}`}>
+            <button className="project-card-button">VER MÁS</button>
+          </Link>
         </div>
       </article>
     </>
