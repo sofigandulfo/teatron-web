246f0a1dceb64a4f43f27b1c551b9b5e6e7dcd49
sofigandulfo <sofigandulfo@gmail.com>
2024-06-26 23:54:51 -0300

    #

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Faq from "./pages/Faq";
 import Reservation from "./pages/Reservation";
 import SignIn from "./pages/SignIn";
 import Register from "./pages/Register";
-import MyReservations from "./pages/MyReservations";
 import ProjectDetail from './pages/ProjectDetail'
 import Header from './components/Header/Header';
 import Footer from "./components/Footer/Footer";
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -17,21 +17,19 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <ScrollToTop />
         <Header />
         <Routes>
-          <Route path="/" exact element={<Home />}></Route>
-          <Route path="/lineup" element={<Lineup />}></Route>
-          <Route path="/informacion" element={<Faq />}></Route>
-          {/* <Route path="/reservas" element={<Reservation />}></Route> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/lineup" element={<Lineup />} />
+          <Route path="/informacion" element={<Faq />} />
           <Route path="/reservar/:projectName" element={<Reservation />} />
-          <Route path="/login" element={<SignIn />}></Route>
-          <Route path="/registro" element={<Register />}></Route>
-          <Route path="/mis-reservas" element={<MyReservations />}></Route>
-          <Route path="/proyectos/:projectName" element={<ProjectDetail/>}></Route>
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/registro" element={<Register />} />
+          <Route path="/proyectos/:projectName" element={<ProjectDetail />} />
         </Routes>
         <Footer />
       </div>
-      
     </Router>
   );
 }
