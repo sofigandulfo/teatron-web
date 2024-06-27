5673af5c8db7c41fedb1366f2e877965913d9806
sofigandulfo <sofigandulfo@gmail.com>
2024-06-26 23:48:44 -0300

    Lineup + estilos + responsive

--- src/pages/Lineup.js
+++ src/pages/Lineup.js
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 import proyectos from "../data/proyectos.json";
 import "../styles/Lineup.css";
 
@@ -6,28 +6,30 @@ function Lineup() {
   return (
     <>
       <h1 className="lineup-title">LINEUP</h1>
-      <article className="lineup">
+      <div className="lineup">
         <table className="lineup-table">
-          <thead className="lineup-tablehead">
+          {/* <thead className="lineup-tablehead">
             <tr className="lineup-tablerow-head">
               <th className="lineup-th">HORA</th>
               <th className="lineup-th">PROYECTO</th>
               <th className="lineup-th">ESPACIO</th>
             </tr>
-          </thead >
+          </thead> */}
           <tbody className="lineup-tablebody">
             {proyectos.map((project) => (
-
               <tr key={project.id} className="lineup-tablerow">
                 <td className="lineup-td">{project.hora}</td>
-                <td className="lineup-td">{project.nombre}</td>
+                <td className="lineup-td">
+                  <Link to={`/proyectos/${encodeURIComponent(project.nombre)}`}>
+                    {project.nombre}
+                  </Link>
+                </td>
                 <td className="lineup-td">{project.espacio}</td>
               </tr>
-
             ))}
           </tbody>
         </table>
-      </article>
+      </div>
     </>
   );
 }
