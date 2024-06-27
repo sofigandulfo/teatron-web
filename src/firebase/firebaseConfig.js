54518a45ed0f20817825784ccad8f3a1b750685c
sofigandulfo <sofigandulfo@gmail.com>
2024-06-26 23:58:11 -0300

    #

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6S5MvpmE0bRO9b-m7pAgdrqJ3oGIgf0M",
-  authDomain: "teatron-2024.firebaseapp.com",
-  projectId: "teatron-2024",
-  storageBucket: "teatron-2024.appspot.com",
-  messagingSenderId: "427073598822",
-  appId: "1:427073598822:web:281f2a4b08278b1045c4a7",
-  measurementId: "G-30CYQBK1J0",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
