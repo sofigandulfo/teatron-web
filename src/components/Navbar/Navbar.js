1a0d7c6eb3f4676d30f61694ee4e3523c6a7b137
sofigandulfo <sofigandulfo@gmail.com>
2024-06-26 23:53:31 -0300

    Navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,46 +1,76 @@
-import { useState } from "react"
-import { NavLink } from "react-router-dom"
+import { useState, useEffect } from 'react';
+import { NavLink, useNavigate } from "react-router-dom";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth } from '../../firebase/firebaseConfig';
+import "../../styles/Header.css";
 
 function Navbar() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setLoggedIn(!!user); // Actualiza el estado de inicio de sesión según el usuario autenticado
+    });
 
-  const handleLogout = () => {
-    setLoggedIn(false);
-    setMenuOpen(false);
-  }
+    return () => unsubscribe();
+  }, []);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setLoggedIn(false); // Actualiza el estado de inicio de sesión a false
+      handleMenuClose();
+      navigate('/login'); // Redirige al usuario a la página principal
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <nav>
+    <nav className="navbar">
       <ul>
-        <li><NavLink to="/lineup" className={({ isActive }) => (isActive ? 'active' : '')}>LINEUP</NavLink></li>
-        <li><NavLink to="/informacion" className={({ isActive }) => (isActive ? 'active' : '')}>INFORMACIÓN</NavLink></li>
-        {/* <li><NavLink to="/reservas" className={({ isActive }) => (isActive ? 'active' : '')}>RESERVAS</NavLink></li> */}
         <li>
-          <button onClick={toggleMenu}>USER</button>
-          {menuOpen && (
-            <div className="user-menu">
-              {loggedIn ? (
-                <>
-                  <NavLink to="/mis-reservas">MIS RESERVAS</NavLink>
-                  <button onClick={handleLogout}>CERRAR SESIÓN</button>
-                </>
-              ) : (
-                <>
-                  <NavLink to="/login">INICIAR SESIÓN</NavLink>
-                  <NavLink to="/registro">REGISTRARSE</NavLink>
-                </>
-              )}
-            </div>
-          )}
+          <NavLink to="/lineup" activeClassName="active">LINEUP</NavLink>
+        </li>
+        <li>
+          <NavLink to="/informacion" activeClassName="active">INFORMACIÓN</NavLink>
+        </li>
+        <li className="user-menu-container">
+          <IconButton onClick={handleMenuOpen} className="user-button">
+            <AccountCircleIcon style={{ fontSize: 40, color: 'white' }} />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            {loggedIn ? (
+              <MenuItem onClick={handleLogout} >CERRAR SESIÓN</MenuItem>
+            ) : (
+              <>
+                <MenuItem component={NavLink} to="/login">INICIAR SESIÓN</MenuItem>
+                <MenuItem component={NavLink} to="/registro">REGISTRARSE</MenuItem>
+              </>
+            )}
+          </Menu>
         </li>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
